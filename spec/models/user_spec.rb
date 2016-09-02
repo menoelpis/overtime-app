14e8e53e737e10795bb5e0f2b11a181f9986697a
Daniel Park <menoelpis@gmail.com>
2016-09-02 10:12:19 +0900

    Created index action for posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "creation" do 
+  describe 'Creation' do 
    
     before do
       @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
     end
 
-    it "can be created" do
+    it 'can be created' do
       expect(@user).to be_valid
     end
 
-    it "can not be created without first_name, last_name" do
+    it 'can not be created without first_name, last_name' do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
