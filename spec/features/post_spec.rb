802a3263ae395573dd662245e6628e4ed61ffe8f
Daniel Park <menoelpis@gmail.com>
2016-09-11 15:56:32 +0900

    Separated Post Index Table File

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -7,15 +7,21 @@ describe 'navigate' do
   end
 
   describe 'index' do
-    it 'can be reached successfully' do
+    before do
       visit posts_path
+    end
+
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
 
-    it 'has a title of Posts' do
-      visit posts_path
-      expect(page).to have_content(/Posts/)
-    end
+    # it 'has a list of posts' do 
+    #   Post.create(date: Date.today, rationale: "Post1")
+    #   Post.create(date: Date.today, rationale: "Post2")
+    #   visit posts_path
+    #   expect(page).to have_content(/Post1|Post2/)
+    # end
+
   end
 
   describe 'creation' do
